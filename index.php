1b0bfb59173826bd504a88dde6bd5386f145f5aa
Саргылаана <hovalanshi@gmail.com>
2017-12-12 15:31:34 +1000

    авторизация и главное меню

--- index.php
+++ index.php
@@ -1,5 +1,92 @@
 <?
 session_start();
+ /* if    (isset($_POST['login'])) 
+        { 
+            $login = str_replace(" ","",$_POST['login']); 
+            if ($login == '') 
+            { 
+                unset($login);
+            }    
+        } 
+        //заносим введенный пользователем e-mail, если он    пустой, то уничтожаем переменную
+        if    (isset($_POST['password'])) 
+        { 
+            $password = str_replace(" ","",$_POST['password']);
+            if ($password == '') 
+            { 
+                unset($password);
+            } 
+        } 
+        //если пользователь не ввел логин или пароль, то выдаем ошибку и останавливаем скрипт
+        if (empty($login) or empty($password)) 
+        { //если пользователь не ввел логин или пароль, то выдаем ошибку и останавливаем скрипт
+            ?>
+            <script>
+                alert("Вы ввели не всю информацию, вернитесь назад и заполните все поля!");
+            </script>
+            <?
+            return false;
+        }
+        else
+        {
+            $dsn = 'mysql:dbname=practice;host=127.0.0.1;port=3306;charset=utf8';
+            $opt = [
+                PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                PDO::ATTR_EMULATE_PREPARES   => false,
+            ];
+             
+            $pdo = new PDO($dsn, 'root', '', $opt);
+            $sql="SELECT * FROM users WHERE login=?";
+            
+            $stm = $pdo->prepare($sql);
+            $stm->execute([$login]);
+            $res = $stm->fetch();
+            if (!$res)
+            {
+                ?>
+                <script>
+                    alert("Извините, введённый вами логин неверный.");
+                </script>
+                <?
+                        return false;
+            } 
+            else
+            { //если существует, то сверяем пароли
+                $password=md5($password);
+                if ($res['password']==$password) 
+                {
+                    if($res['activate']==0)
+                    {
+                        ?>
+                        <script>
+                            alert("Извините, ваша учётная запись ещё не подтверждена администратором сайта, попробуйте позже!");
+                        </script>
+                        <?
+                        return false;
+                    }
+                    else
+                    {
+                        //если пароли совпадают, то запускаем пользователю сессию! Можете его поздравить, он вошел!
+                        $_SESSION['login']=$res['login']; 
+                        $_SESSION['password']=$res['password']; 
+                        $_SESSION['id']=$res['id'];
+                        $_SESSION['role']=$res['role'];//эти данные очень часто используются, вот их и будет "носить с собой" вошедший пользователь
+                        $_SESSION['activate']=$res['activate'];
+                        return true;
+                    }
+                }
+                else 
+                {
+                    ?>
+                    <script>
+                        alert("Извините, введённый пароль неверный.");
+                    </script>
+                    <?
+                        return false;
+                }
+            }
+        }   */
 ?>
 <!doctype html>
 <html class="no-js" lang="ru" dir="ltr">
@@ -12,29 +99,23 @@ session_start();
   </head>
   <body>  
       <?php include ("menu.php");?>
-        <ul class="accordion shade" data-accordion data-allow-all-closed="true">
-          <li class="accordion-item" data-accordion-item>
-            <a href="#" class="accordion-title">Accordion 1</a>
-            <div class="accordion-content" data-tab-content>
-              <p>Panel 1. Lorem ipsum dolor</p>
-              <a href="#">Nowhere to Go</a>
-            </div>
-          </li>
-        </ul>
-        <ul class="accordion shade" data-accordion data-allow-all-closed="true">
-            <li class="accordion-item" data-accordion-item>
-                <a class="accordion-title">Узнай основную информацию о практике</a>
-                <div class="accordion-content" data-tab-content>
-                  <p>Panel 1. Lorem ipsum dolor</p>
-                </div>
-            </li>
-        </ul>
-                <a class="accordion-title" style="content:'25.11.2017-25.12.2017'">Название компании</a>
-                <div>
-                  <p>Panel 1. Lorem ipsum dolor</p>
-                  <a href="#" class="button float-right">Learn More</a>
-                </div>
+        <a class="accordion-title">Узнай основную информацию о практике</a>
+        <a id="0b" onclick="is_clicked('0')" style="padding: 9px;" class="button login">Регистрация</a>
+        <div id="0">
+          <p>Panel 1. Lorem ipsum dolor</p>
+        </div>
+        <a class="accordion-title">Название компании</a>
+        <div>
+          <p>Panel 1. Lorem ipsum dolor</p>
+          <a href="#" class="button float-right">Learn More</a>
+        </div>
     </div>
     <?php include ("footer.php");?>
+    <script>
+            function is_clicked(id){
+            document.getElementById(id).style.display='block'; 
+            document.getElementById(id+'b').style['background-color']='#d8d8d8'; 
+         }
+    </script>
   </body>
 </html>
--- menu.php
+++ menu.php
@@ -22,93 +22,122 @@ class AuthClass {
         else return false; //Пользователь не авторизован, т.к. переменная is_auth не создана
     }
     public function auth($login, $passwors) {
-	if    (isset($_POST['login'])) 
-	{ 
-		$login = str_replace(" ","",$_POST['login']); 
-		if ($login == '') 
-		{ 
-			unset($login);
-		}    
-	} 
-	//заносим введенный пользователем e-mail, если он    пустой, то уничтожаем переменную
-	if    (isset($_POST['password'])) 
-	{ 
-		$password = str_replace(" ","",$_POST['password']);
-		if ($password == '') 
-		{ 
-			unset($password);
-		} 
-	} 
-	//если пользователь не ввел логин или пароль, то выдаем ошибку и останавливаем скрипт
-	if (empty($login) or empty($password)) 
-	{ //если пользователь не ввел логин или пароль, то выдаем ошибку и останавливаем скрипт
-		?>
-		<script>
-			alert("Вы ввели не всю информацию, вернитесь назад и заполните все поля!");
-		</script>
-		<?
-        return false;
-	}
-	else
-	{
-		$dsn = 'mysql:dbname=practice;host=127.0.0.1;port=3306;charset=utf8';
-		$opt = [
-			PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-			PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-			PDO::ATTR_EMULATE_PREPARES   => false,
-		];
-		 
-		$pdo = new PDO($dsn, 'root', '', $opt);
-		$sql="SELECT * FROM users WHERE login=?";
-		
-		$stm = $pdo->prepare($sql);
-		$stm->execute([$login]);
-		$res = $stm->fetch();
-		if (!$res)
-		{
-			?>
-			<script>
-				alert("Извините, введённый вами логин неверный.");
-			</script>
-			<?
-                    return false;
-		} 
-		else
-		{ //если существует, то сверяем пароли
-			$password=md5($password);
-			if ($res['password']==$password) 
-			{
-				if($res['activate']==0)
-				{
-					?>
-					<script>
-						alert("Извините, ваша учётная запись ещё не подтверждена администратором сайта, попробуйте позже!");
-					</script>
-					<?
-                    return false;
-				}
-				else
-				{
-					//если пароли совпадают, то запускаем пользователю сессию! Можете его поздравить, он вошел!
-					$_SESSION['login']=$res['login']; 
-					$_SESSION['password']=$res['password']; 
-					$_SESSION['id']=$res['id'];
-					$_SESSION['role']=$res['role'];//эти данные очень часто используются, вот их и будет "носить с собой" вошедший пользователь
-					$_SESSION['activate']=$res['activate'];
-                    return true;
-				}
-			}
-			else 
-			{
-				?>
-				<script>
-					alert("Извините, введённый пароль неверный.");
-				</script>
-				<?
-                    return false;
-			}
-		}
-	}   
+        if    (isset($_POST['login'])) 
+        { 
+            $login = str_replace(" ","",$_POST['login']); 
+            if ($login == '') 
+            { 
+                unset($login);
+            }    
+        } 
+        //заносим введенный пользователем e-mail, если он    пустой, то уничтожаем переменную
+        if    (isset($_POST['password'])) 
+        { 
+            $password = str_replace(" ","",$_POST['password']);
+            if ($password == '') 
+            { 
+                unset($password);
+            } 
+        } 
+        //если пользователь не ввел логин или пароль, то выдаем ошибку и останавливаем скрипт
+        if (empty($login) or empty($password)) 
+        { //если пользователь не ввел логин или пароль, то выдаем ошибку и останавливаем скрипт
+            ?>
+            <script>
+                alert("Вы ввели не всю информацию, вернитесь назад и заполните все поля!");
+            </script>
+            <?
+            $_SESSION["is_auth"]=false;
+            return false;
+        }
+        else
+        {
+            $dsn = 'mysql:dbname=practice;host=127.0.0.1;port=3306;charset=utf8';
+            $opt = [
+                PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                PDO::ATTR_EMULATE_PREPARES   => false,
+            ];
+             
+            $pdo = new PDO($dsn, 'root', '', $opt);
+            $sql="SELECT * FROM users WHERE login=?";
+            
+            $stm = $pdo->prepare($sql);
+            $stm->execute([$login]);
+            $res = $stm->fetch();
+            if (!$res)
+            {
+                ?>
+                <script>
+                    alert("Извините, введённый вами логин неверный.");
+                </script>
+                <?
+                        $_SESSION["is_auth"]=false;
+                        return false;
+            } 
+            else
+            { //если существует, то сверяем пароли
+                $password=md5($password);
+                if ($res['password']==$password) 
+                {
+                    if($res['activate']==0)
+                    {
+                        ?>
+                        <script>
+                            alert("Извините, ваша учётная запись ещё не подтверждена администратором сайта, попробуйте позже!");
+                        </script>
+                        <?
+                        $_SESSION["is_auth"]=false;
+                        return false;
+                    }
+                    else
+                    {
+                        $_SESSION["is_auth"]=true;
+                        //если пароли совпадают, то запускаем пользователю сессию! Можете его поздравить, он вошел!
+                        $_SESSION['login']=$res['login']; 
+                        $_SESSION['password']=$res['password']; 
+                        $_SESSION['id']=$res['id'];
+                        $_SESSION['role']=$res['role'];//эти данные очень часто используются, вот их и будет "носить с собой" вошедший пользователь
+                        $_SESSION['activate']=$res['activate'];
+                        switch($_SESSION['role']){
+                              case 0:
+                                    $_SESSION['name']='Админ';
+                              break;
+                              case 1:
+                                $sql="SELECT fio FROM leader WHERE id=?";
+                                $stm = $pdo->prepare($sql);
+                                $stm->execute([$_SESSION['id']]);
+                                $res = $stm->fetch();
+                                $_SESSION['name']=$res['fio'];
+                                echo $_SESSION['name'];
+                              break;
+                              case 2:
+                                $sql="SELECT fio FROM student WHERE id=?";
+                                $stm = $pdo->prepare($sql);
+                                $stm->execute([$_SESSION['id']]);
+                                $res = $stm->fetch();
+                                $_SESSION['name']=$res['fio'];
+                                echo $_SESSION['name'];
+                              break;
+                              case 3:
+                                $_SESSION['name']='Предприятие';
+                              break;
+                        }
+                        return true;
+                    }
+                }
+                else 
+                {
+                    ?>
+                    <script>
+                        alert("Извините, введённый пароль неверный.");
+                    </script>
+                    <?
+                        $_SESSION["is_auth"]=false;
+                        return false;
+                }
+            }
+        }   
     }
      
     /**
@@ -130,21 +159,20 @@ class AuthClass {
     public function out() {
         $_SESSION = array(); //Очищаем сессию
         session_destroy(); //Уничтожаем
+		exit("<html><head><meta http-equiv='Refresh' content='0; URL=..".$_SERVER['PHP_SELF']."'></head></html>");
     }
 }
 
 $auth = new AuthClass();
- 
-if (isset($_POST["login"]) && isset($_POST["password"])) { //Если логин и пароль были отправлены
+if ($_POST["login"] && $_POST["password"]) { //Если логин и пароль были отправлены
     if (!$auth->auth($_POST["login"], $_POST["password"])) { //Если логин и пароль введен не правильно
 		exit("<html><head><meta http-equiv='Refresh' content='0; URL=..".$_SERVER['PHP_SELF']."'></head></html>");
     }
     else{
         echo "<script>alert('Вы авторизованы');</script>";
-        
+		echo "<script>window.location.href='".$_SERVER['PHP_SELF']."'</script>";//ну тип
     }
 }
- 
 if (isset($_GET["is_exit"])) { //Если нажата кнопка выхода
     if ($_GET["is_exit"] == 1) {
         $auth->out(); //Выходим
@@ -170,7 +198,25 @@ if (isset($_GET["is_exit"])) { //Если нажата кнопка выхода
     <?php include ("header.php");?>
     <div class="top-bar shade" id="responsive-menu">
         <ul class="menu">
-          <li><a class="button top">Гость</a></li>
+          <li><a class="button top"><? if($_SESSION['is_auth']){
+                                            switch ($_SESSION['role']){
+                                              case 0:
+                                                echo 'Администратор';
+                                              break;
+                                              case 1:
+                                                echo 'Руководитель';
+                                              break;
+                                              case 2:
+                                                echo 'Студент';
+                                              break;
+                                              case 3:
+                                                echo 'Предприятие';
+                                              break;
+                                            }         
+                                        }
+                                        else
+                                            echo 'Гость';
+                                      ?></a></li>
           <?
                 $path=parse_url($_SERVER["HTTP_HOST"].$_SERVER["REQUEST_URI"],PHP_URL_PATH);
                 if ($path=="practise/"||$path=="practise/index.php"){
@@ -181,7 +227,14 @@ if (isset($_GET["is_exit"])) { //Если нажата кнопка выхода
           ?>
         </ul>
         <ul class="menu align-right">
-          <li><a id="openD" class="button top">Войти</a></li>
+          <li><a id="openD" class="button top"><?
+          if ($_GET['name'])
+            echo $_GET['name'];
+          else if($_SESSION['is_auth'])
+            echo $_SESSION['name'];
+          else 
+            echo 'Войти'; 
+          ?></a></li>
         </ul>
       </div>
 <script>
@@ -201,4 +254,8 @@ if (isset($_GET["is_exit"])) { //Если нажата кнопка выхода
                 "filter": "none"
             });
         });
+        
+    window.onload = function(){
+        
+    }
 </script>
