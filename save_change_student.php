a590801d164acdd3266a01f986e6538b925c55e0
Тимофей <stelth-assasin@mail.ru>
2017-12-20 15:04:00 +1000

    Все что есть

--- save_change_student.php
+++ save_change_student.php
@@ -1,5 +1,5 @@
 <?php
-
+   session_start();
     if (isset($_POST['login'])) { $login = $_POST['login']; if ($login == '') { unset($login);} } //заносим введенный пользователем логин в переменную $login, если он пустой, то уничтожаем переменную
     if (isset($_POST['password'])) { $password=$_POST['password']; if ($password =='') { unset($password);} }
 	$checkPassword = $_POST['checkPassword'];
