9657859880f66f17637e7c9b4cd5e05cfcf4c98a
Саргылаана <hovalanshi@gmail.com>
2018-01-17 09:24:57 +1000

    Исправление

--- profile.php
+++ profile.php
@@ -323,7 +323,7 @@ if ($id>0){
               <b>Номер телефона: </b>'.$res['phone'].'</br>
               <b>Почта: </b>'.$res['email'].'</br>
               <b>Группы: </b>');
-            $stds=explode(";",$row['gropus']);
+            $stds=explode(";",$row['groups']);
             foreach($stds as $gr){
                 $sql='SELECT number, name FROM `group` WHERE id=?';
                 $stm = $pdo->prepare($sql);
